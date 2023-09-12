53359fdb02a5262dda6222a1cd65f67d0853c2cb
zeeshi2k1 <zeeshan.spidey2@gmail.com>
2023-09-12 13:54:47 +0500

    Initiat commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme:ThemeData.dark().copyWith(
+        appBarTheme: AppBarTheme(
+          color: Color(0xFF0A0D22),
+        ),
+        scaffoldBackgroundColor: Color(0xFF0A0D22),
+      ),
+      home: InputPage(),
+    );
+  }
+}
+
